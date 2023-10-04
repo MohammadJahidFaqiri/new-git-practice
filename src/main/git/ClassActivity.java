ec69c5bb52d7be65b4aa30a9919d32409ebdc962
Mohammad Jahid Faqiri <jahid.faqiri@gmail.com>
2023-10-04 16:57:12 -0400

    new changes

--- src/main/git/ClassActivity.java
+++ src/main/git/ClassActivity.java
@@ -4,7 +4,9 @@ public class ClassActivity {
     public static void main(String[] args) {
         System.out.println("Activity Class");
         System.out.println("Created by Jahid");
-        
-        
+
+        String firstName = "Jahid";
+        String lastName = "Faqiri";
+        System.out.print("First Name - "+firstName + "\n"+"Last Name - "+ lastName);
     }
 }
