ef87d37eea75d902b4fe93ac459668b9128c34b3
Jahid <146789412+MohammadJahidFaqiri@users.noreply.github.com>
2023-10-04 16:47:26 -0400

    Update ClassActivity.java

--- src/main/git/ClassActivity.java
+++ src/main/git/ClassActivity.java
@@ -3,5 +3,8 @@ package main.git;
 public class ClassActivity {
     public static void main(String[] args) {
         System.out.println("Activity Class");
+        System.out.println("Created by Jahid");
+        
+        
     }
 }
